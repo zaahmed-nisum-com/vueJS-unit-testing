1c678c9c0ed7ee47265c1db4f5a0c2656db8bd5f
Zain Ahmed <zain.ahmed@backcountry.com>
2021-11-15 18:10:10 +0500

    update fiels

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <!-- <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <!-- <Header /> -->
+    <Header />
     <!-- <Home /> -->
     <ToDo title="My List" />
   </div>
--- tests/unit/components/Header.spec.js
+++ tests/unit/components/Header.spec.js
@@ -1,26 +1,25 @@
-import { createLocalVue, mount, shallowMount } from '@vue/test-utils'
-import Header from '../../../src/components/Header'
+import { createLocalVue, mount, shallowMount } from "@vue/test-utils";
+import Header from "../../../src/components/Header";
 
-describe('header.vue', () => {
-
-    test('snapshot', () => {
-        const wrapper = mount(Header)
-        expect(wrapper.find('div')).toMatchSnapshot(true)
-    })
-
-    let wrapper;
-    beforeEach(() => {
-        const localVue = createLocalVue()
-        wrapper = shallowMount(Header, {
-            localVue
-        });
-    })
-
-    test('renders a vue instance', () => {
-        expect(mount(Header).isVueInstance()).toBe(true);
+describe("header.vue", () => {
+  let wrapper;
+  beforeEach(() => {
+    const localVue = createLocalVue();
+    wrapper = shallowMount(Header, {
+      localVue,
     });
+  });
 
-    test('Checks the data-title', () => {
-        expect('Weather App').toMatch('Weather App')
-    })
-})
+  test("renders a vue instance", () => {
+    expect(mount(Header).isVueInstance()).toBe(true);
+  });
+
+  test("snapshot", () => {
+    const wrapper = mount(Header);
+    expect(wrapper.find("div")).toMatchSnapshot(true);
+  });
+
+  test("Checks the data-title", () => {
+    expect("Weather App").toMatch("Weather App");
+  });
+});
